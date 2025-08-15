78229e9542698f30b15ea02e5e96c7a68541a0b5
wpmg <wilmer.prentius@slu.se>
2025-08-15 17:45:49 +0200

    (release) rpkg samplr 0.1.0 (#14)

---
+++ rpkg/samplr/R/balanced_sampling.R
@@ -0,0 +1,94 @@
+#' Balanced sampling
+#'
+#' @name Balanced sampling
+#' @rdname balanced_sampling
+#' @description
+#' Selects balanced samples with prescribed inclusion probabilities from finite populations.
+#'
+#' @details
+#' For the `cube` method, a fixed sized sample is obtained if the first column of `balance_mat` is
+#' the inclusion probabilities. For `cube_stratified`, the inclusion probabilities are inserted
+#' automatically.
+#'
+#' @param probabilities A vector of inclusion probabilities.
+#' @param balance_mat A matrix of balancing covariates.
+#' @param strata An integer vector with stratum numbers for each unit.
+#' @inheritDotParams .sampling_defaults -max_iter -bucket_size
+#'
+#' @references
+#' Deville, J. C. and Tillé, Y. (2004).
+#' Efficient balanced sampling: the cube method.
+#' Biometrika, 91(4), 893-912.
+#'
+#' Chauvet, G. and Tillé, Y. (2006).
+#' A fast algorithm for balanced sampling.
+#' Computational Statistics, 21(1), 53-62.
+#'
+#' Chauvet, G. (2009).
+#' Stratified balanced sampling.
+#' Survey Methodology, 35, 115-119.
+#'
+#' @examples
+#' \dontrun{
+#' set.seed(12345);
+#' N = 1000;
+#' n = 100;
+#' prob = rep(n/N, N);
+#' xb = matrix(c(prob, runif(N * 2)), ncol = 3);
+#' strata = c(rep(1L, 100), rep(2L, 200), rep(3L, 300), rep(4L, 400));
+#'
+#' s = cube(prob, xb);
+#' plot(xb[, 2], xb[, 3], pch = ifelse(sample_to_indicator(s, N), 19, 1));
+#'
+#' s = cube_stratified(prob, xb[, -1], strata);
+#' plot(xb[, 2], xb[, 3], pch = ifelse(sample_to_indicator(s, N), 19, 1));
+#'
+#' # Respects inclusion probabilities
+#' set.seed(12345);
+#' prob = c(0.2, 0.25, 0.35, 0.4, 0.5, 0.5, 0.55, 0.65, 0.7, 0.9);
+#' N = length(prob);
+#' xb = matrix(c(prob, runif(N * 2)), ncol = 3);
+#'
+#' ep = rep(0L, N);
+#' r = 10000L;
+#'
+#' for (i in seq_len(r)) {
+#'   s = cube(prob, xb);
+#'   ep[s] = ep[s] + 1L;
+#' }
+#'
+#' print(ep / r - prob);
+#' }
+#'
+NULL
+
+.balanced_wrapper = function(method, probabilities, balance_mat, ...) {
+  args = .sampling_defaults(...);
+  rust_balanced(
+    as.double(probabilities),
+    as.matrix(balance_mat),
+    args$eps,
+    args$seed,
+    method
+  )
+}
+
+#' @describeIn balanced_sampling The cube method
+#' @export
+cube = function(probabilities, balance_mat, ...) {
+  .balanced_wrapper("cube", probabilities, balance_mat, ...)
+}
+
+#' @describeIn balanced_sampling The stratified cube method
+#' @export
+cube_stratified = function(probabilities, balance_mat, strata, ...) {
+  args = .sampling_defaults(...);
+  rust_balanced_stratified(
+    as.numeric(probabilities),
+    as.matrix(balance_mat),
+    as.integer(strata),
+    args$eps,
+    args$seed,
+    "cube"
+  )
+}
---
+++ rpkg/samplr/R/pips_from_vector.R
@@ -0,0 +1,41 @@
+#' Inclusion probabilities proportional-to-size
+#'
+#' @description
+#' Computes the first-order inclusion probabilities from a vector of positive numbers,
+#' for an inclusion probabilities proportional-to-size design.
+#'
+#' @param values A vector of positive numbers
+#' @param sample_size The wanted sample size
+#'
+#' @return A vector of inclusion probabilities proportional-to-size
+#'
+#' @examples
+#' \dontrun{
+#' set.seed(12345);
+#' N = 1000;
+#' n = 100;
+#' x = matrix(runif(N * 2), ncol = 2);
+#' prob = pips_from_vector(x[, 1], n);
+#' s = lpm_2(prob, x);
+#' plot(x[, 1], x[, 2], pch = ifelse(sample_to_indicator(s, N), 19, 1));
+#' }
+#'
+#' @export
+pips_from_vector = function(values, sample_size) {
+  values = as.double(values);
+  N = length(values);
+  sample_size = as.integer(sample_size);
+
+  if (length(sample_size) != 1 || sample_size > N || sample_size < 0)
+    stop("n must be integer in [0, N]");
+
+  if (sample_size == N)
+    return(rep(1.0, N));
+  if (sample_size == 0)
+    return(rep(0.0, N));
+
+  rust_pips_from_values(
+    values,
+    sample_size
+  )
+}
