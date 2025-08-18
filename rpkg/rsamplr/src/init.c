2362f6e4df4153bb716da66595232823ac73f98a
wpmg <wilmer.prentius@slu.se>
2025-08-18 10:31:37 +0200

    Renamed to rsamplr

--- rpkg/rsamplr/src/init.c
+++ rpkg/rsamplr/src/init.c
@@ -111,7 +111,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-void R_init_samplr(DllInfo *dll) {
+void R_init_rsamplr(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 
