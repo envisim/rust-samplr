16cd401b520cd8e3b6d9dc0e15cf0f4731984e5d
wpmg <wilmer.prentius@slu.se>
2025-08-26 14:07:29 +0200

    Remove seed from doc

--- rpkg/rsamplr/R/utils.R
+++ rpkg/rsamplr/R/utils.R
@@ -1,7 +1,6 @@
 #' Sampling defaults
 #'
 #' @param eps A small value used when comparing floats.
-#' @param seed An integer used to determine the seed for the random number generator.
 #' @param max_iter The maximum number of iterations used in iterative algorithms.
 #' @param bucket_size The maximum size of the k-d-tree nodes. A higher value gives a slower
 #' k-d-tree, but is faster to create and takes up less memory.
