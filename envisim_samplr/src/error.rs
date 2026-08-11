2044450bbf45e9833b5598ab9356061bc160b540
Wilmer Prentius <wilmer.prentius@slu.se>
2026-08-11 14:11:11 +0200

    (feat) Reintroduce thiserror dependency (#48)
    - Reintroduces `thiserror` for error handling (removed in #6). 
    - Moves just-files to root folder.

--- envisim_samplr/src/error.rs
+++ envisim_samplr/src/error.rs
@@ -15,72 +15,28 @@
 use std::num::NonZeroUsize;
 
 use envisim_utils::sampling_options::SamplingOptionsError;
+use thiserror::Error;
 
 /// Sampling related error types
 #[non_exhaustive]
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum SamplingError {
-    Options(SamplingOptionsError),
+    #[error("SamplingOptionsError: {0}")]
+    Options(#[from] SamplingOptionsError),
     // max iterations reached
+    #[error("max iterations ({0}) reached")]
     MaxIterations(NonZeroUsize),
+    #[error("invalid stratification")]
     IncorrectStratification,
+    #[error("draw probabilities must sum nominally to 1.0")]
     IncorrectDrawProbabilities,
+    #[error("probabilities must sum to a nominal integer value")]
     IncorrectProbabilitiesIntegerSum,
+    #[error("annealing temperature must be positive")]
     IncorrectAnnealingTemperature,
+    #[error("annealing temperature cooling rate must be in (0.0, 1.0)")]
     IncorrectAnnealingRate,
+    #[error("sample size must be positive")]
     ZeroSampleSize,
 }
 pub type SamplingResult<T> = Result<T, SamplingError>;
-
-#[expect(clippy::absolute_paths, reason = "possible override")]
-impl std::error::Error for SamplingError {
-    #[inline]
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        #[expect(clippy::wildcard_enum_match_arm, reason = "Options is a special case")]
-        match self {
-            SamplingError::Options(err) => Some(err),
-            _ => None,
-        }
-    }
-}
-
-#[expect(clippy::absolute_paths, reason = "possible override")]
-impl std::fmt::Display for SamplingError {
-    #[expect(clippy::enum_glob_use, reason = "handy to use in a match")]
-    #[inline]
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use SamplingError::*;
-        match self {
-            Options(err) => err.fmt(f),
-            MaxIterations(max_iter) => {
-                write!(f, "max iterations ({max_iter}) reached")
-            }
-            IncorrectStratification => {
-                write!(f, "incorrect stratification")
-            }
-            IncorrectDrawProbabilities => {
-                write!(f, "draw probabilities must sum to 1.0")
-            }
-            IncorrectProbabilitiesIntegerSum => {
-                write!(f, "probabilities should sum to integer value")
-            }
-            IncorrectAnnealingTemperature => {
-                write!(f, "annealing temperature must be positive")
-            }
-            IncorrectAnnealingRate => {
-                write!(
-                    f,
-                    "annealing temperature cooling rate must be in (0.0, 1.0)"
-                )
-            }
-            ZeroSampleSize => {
-                write!(f, "sample size must be positive")
-            }
-        }
-    }
-}
-
-impl From<SamplingOptionsError> for SamplingError {
-    #[inline]
-    fn from(err: SamplingOptionsError) -> Self { SamplingError::Options(err) }
-}
--- envisim_utils/src/sampling_options/error.rs
+++ envisim_utils/src/sampling_options/error.rs
@@ -12,46 +12,34 @@
 
 //! Sampling options errors
 
+use thiserror::Error;
+
 #[non_exhaustive]
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum SamplingOptionsError {
+    #[error("eps must be in [0.0, 1.0)")]
     InvalidEpsilon,
+    #[error("population size must be positive")]
     InvalidPopulationSize,
+    #[error("sample contains invalid units")]
     InvalidSample,
+    #[error("sample size must not be larger than the population size")]
     InvalidSampleSize,
+    #[error("probabilities must be valid (nominally in [0.0, 1.0])")]
     InvalidProbability,
+    #[error("the number of random values must not be smaller than the population size")]
     InvalidRandomValues,
+    #[error("iterations must be positive")]
     InvalidIterations,
+    #[error("the number of units in the spreading data must match the population size")]
     InvalidSpreading,
+    #[error("bucket size must be at least 1")]
     InvalidBucketSize,
+    #[error("the number of units the balancing data must match the population size")]
     InvalidBalancing,
+    #[error("spreading data must be provided")]
     MissingSpreading,
+    #[error("balancing data must be provided")]
     MissingBalancing,
 }
 pub type SamplingOptionsResult<T> = Result<T, SamplingOptionsError>;
-
-#[expect(clippy::absolute_paths, reason = "possible override")]
-impl std::error::Error for SamplingOptionsError {}
-
-#[expect(clippy::absolute_paths, reason = "possible override")]
-impl std::fmt::Display for SamplingOptionsError {
-    #[expect(clippy::enum_glob_use, reason = "handy to use in a match")]
-    #[inline]
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use SamplingOptionsError::*;
-        match self {
-            InvalidEpsilon => write!(f, "eps must be in [0.0, 1.0)"),
-            InvalidPopulationSize => write!(f, "population is empty"),
-            InvalidSample => write!(f, "sample contains invalid units"),
-            InvalidSampleSize => write!(f, "sample size must be smaller than population size"),
-            InvalidProbability => write!(f, "probabilities must be in [0.0, 1.0]"),
-            InvalidRandomValues => write!(f, "random values length must be >= population size"),
-            InvalidIterations => write!(f, "iterations must be > 0"),
-            InvalidSpreading => write!(f, "spreading matrix must have population_size rows"),
-            InvalidBucketSize => write!(f, "bucket size must be at least 1"),
-            InvalidBalancing => write!(f, "balancing matrix must have population_size rows"),
-            MissingSpreading => write!(f, "no spreading options provided"),
-            MissingBalancing => write!(f, "no balancing options provided"),
-        }
-    }
-}
